ca924fde5a921efb57d13051052230a641bf9f76
vortasystem <vortasystem@gmail.com>
2025-07-24 00:18:03 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+// ضع رابط SheetDB API الخاص بك هنا
+// هذا الرابط سيبقى سريًا على الخادم ولن يظهر للمستخدمين أبدًا
+define('SHEETDB_API_URL', 'https://sheetdb.io/api/v1/5ua8j6rin3svf');
+
+?>
